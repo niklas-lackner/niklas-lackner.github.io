e8489689bdfe4350db84de776b13edf630039be7
niklas-lackner <81580862+niklas-lackner@users.noreply.github.com>
2025-12-31 07:19:12 +0000

    Deploying to gh-pages from @ niklas-lackner/niklas-lackner.github.io@e924aed2f84d02b1c9eb39f4cd2c09eee4f793bf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -76,6 +76,13 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project.html";
             },},{
+        id: 'social-cv',
+        title: 'CV',
+        section: 'Socials',
+        handler: () => {
+          window.open("/assets/pdf/cv.pdf", "_blank");
+        },
+      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
